2bdfd2f064e0174f2e966aebd1a4dbd091b521bd
Darpan Ramani <ramanidarpan3@gmail.com>
2023-06-09 13:32:56 +0530

    Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
-public class Main {
+import org.w3c.dom.ls.LSOutput;
 
+public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly("Darpan");
+        obj.setName("Darpan Ramani");
+        System.out.println(obj.getName());
+    }
 }
